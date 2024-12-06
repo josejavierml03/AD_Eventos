6824c972a9da993e1d7374a9cd34043b0b57938b
juanlax03 <juanlax2003@gmail.com>
2024-12-06 20:03:46 +0100

    vista inicio sesion bean

---
+++ src/main/java/web/LoginBean.java
@@ -0,0 +1,52 @@
+package web;
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@Named("loginBean")
+@SessionScoped
+public class LoginBean implements Serializable {
+
+
+    private String rol;
+    private String usuario;
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String redirigir() {
+        try {
+            if ("propietario".equals(rol)) {
+                FacesContext.getCurrentInstance()
+                .getExternalContext()
+                .redirect("espacios.xhtml?propietario="+usuario);
+            } else if ("organizador".equals(rol)) {
+            	FacesContext.getCurrentInstance()
+                .getExternalContext()
+                .redirect("buscadorEspacios.xhtml");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null; 
+    }
+}
