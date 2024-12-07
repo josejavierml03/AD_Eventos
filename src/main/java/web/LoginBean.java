1ff49022a311db270f18468c0b328fea1ea8f97a
juanlax03 <juanlax2003@gmail.com>
2024-12-07 19:59:49 +0100

    cookies y filtro sesion

--- src/main/java/web/LoginBean.java
+++ src/main/java/web/LoginBean.java
@@ -1,21 +1,57 @@
 package web;
+
 import java.io.IOException;
 import java.io.Serializable;
-
-import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
-@ManagedBean
 @Named("loginBean")
 @SessionScoped
 public class LoginBean implements Serializable {
 
-
     private String rol;
     private String usuario;
+
+    @PostConstruct
+    public void init() {
+        Cookie[] cookies = ((HttpServletRequest) FacesContext.getCurrentInstance()
+                .getExternalContext().getRequest()).getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("usuario".equals(cookie.getName())) {
+                    this.usuario = cookie.getValue();
+                }
+                if ("rol".equals(cookie.getName())) {
+                    this.rol = cookie.getValue();
+                }
+            }
+        }
+    }
+
+    public void verificarSesion() {
+        if (usuario != null && rol != null) {
+            try {
+                if ("propietario".equals(rol)) {
+                    FacesContext.getCurrentInstance()
+                            .getExternalContext()
+                            .redirect("espacios.xhtml?propietario=" + usuario);
+                } else if ("organizador".equals(rol)) {
+                    FacesContext.getCurrentInstance()
+                            .getExternalContext()
+                            .redirect("crearEvento.xhtml");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     public String getRol() {
         return rol;
@@ -35,18 +71,67 @@ public class LoginBean implements Serializable {
 
     public String redirigir() {
         try {
+            guardarSesionEnCookies();
             if ("propietario".equals(rol)) {
                 FacesContext.getCurrentInstance()
-                .getExternalContext()
-                .redirect("espacios.xhtml?propietario="+usuario);
+                        .getExternalContext()
+                        .redirect("espacios.xhtml?propietario=" + usuario);
             } else if ("organizador".equals(rol)) {
-            	FacesContext.getCurrentInstance()
-                .getExternalContext()
-                .redirect("crearEvento.xhtml");
+                FacesContext.getCurrentInstance()
+                        .getExternalContext()
+                        .redirect("crearEvento.xhtml");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null; 
+        return null;
     }
+
+    private void guardarSesionEnCookies() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+        HttpServletRequest request = (HttpServletRequest) facesContext.getExternalContext().getRequest();
+
+        Cookie usuarioCookie = new Cookie("usuario", usuario);
+        usuarioCookie.setMaxAge(60 * 60 * 24 * 7);
+        usuarioCookie.setPath("/");
+
+        Cookie rolCookie = new Cookie("rol", rol);
+        rolCookie.setMaxAge(60 * 60 * 24 * 7);
+        rolCookie.setPath("/");
+
+        response.addCookie(usuarioCookie);
+        response.addCookie(rolCookie);
+
+        request.getSession().setAttribute("usuario", usuario);
+        request.getSession().setAttribute("rol", rol);
+    }
+
+
+    public void cerrarSesion() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+        HttpServletRequest request = (HttpServletRequest) facesContext.getExternalContext().getRequest();
+
+        Cookie usuarioCookie = new Cookie("usuario", null);
+        usuarioCookie.setMaxAge(0);
+        usuarioCookie.setPath("/");
+
+        Cookie rolCookie = new Cookie("rol", null);
+        rolCookie.setMaxAge(0);
+        rolCookie.setPath("/");
+
+        response.addCookie(usuarioCookie);
+        response.addCookie(rolCookie);
+
+        request.getSession().invalidate();
+
+        try {
+            facesContext.getExternalContext().redirect("index.xhtml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
